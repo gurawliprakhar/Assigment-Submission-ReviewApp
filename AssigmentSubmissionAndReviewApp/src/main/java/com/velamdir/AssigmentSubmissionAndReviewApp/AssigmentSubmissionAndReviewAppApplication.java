9f040c3bd0b0baa7274428035a6c405ce5f0a9af
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-14 15:25:39 +0530

    created

---
+++ AssigmentSubmissionAndReviewApp/src/main/java/com/velamdir/AssigmentSubmissionAndReviewApp/AssigmentSubmissionAndReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.velamdir.AssigmentSubmissionAndReviewApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentSubmissionAndReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssigmentSubmissionAndReviewAppApplication.class, args);
+	}
+
+}
---
+++ AssigmentSubmissionAndReviewApp/src/test/java/com/velamdir/AssigmentSubmissionAndReviewApp/AssigmentSubmissionAndReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velamdir.AssigmentSubmissionAndReviewApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentSubmissionAndReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
